d45395c902f83e1ebb49c53e3511f137f6990375
wangfei <wangfei@wangfeideMacBook-Pro.local>
2017-11-29 15:14:52 +0800

    gradle demo2

--- app/src/androidTest/java/com/gavin/androidstudy/demo.groovy
+++ app/src/androidTest/java/com/gavin/androidstudy/demo.groovy
@@ -16,9 +16,21 @@ def sayHi(String c){
 }
 sayHi(a + b + c)
 
+/*
+    字符串：
+        单引号： 同java中的String类型
+        双引号： 可以使用${}
+        三引号： 字符串可以自由换行
+ */
+
+
+println '''我爱你，
+   baby，
+你是否也爱我'''
+
 
 /*
-    闭包：
+    闭包：可以作为参数，也可以作为返回值，也可以直接调用。这个一个特殊的数据类型
 
  */
 
@@ -34,9 +46,29 @@ def closure2 = { p1, p2 ->
 closure1 'fuck'
 println closure2(5, 6)
 
+// 默认参数 it
 def closure3 = {
     println('默认参数it的值：${it}.....')
 }
 closure3('default val')
+
+
+/*
+    增强型List : List和Map都可以用[]表示， 和python类似，使用也很相似  ； 可以自动累加
+        重点： << 左移操作符  ： 向List尾部添加新数据
+
+    range：继承自List 1...100
+ */
+
+def emptyList = []
+def list = ['hello', 100, 'word']
+list[2] = 200
+list << 'fuck'
+println list[2]
+println list.size()
+
+
+def emptyMap = [:]
+def map = ['key1': 100, 'key2':'hello']
 
 
